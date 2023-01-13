3740bf795d6e28785f67d349ec3b641d34694da2
ismailaib <ismailaitbouhmad@gmail.com>
2023-01-13 01:52:16 +0100

    add code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,34 @@
 let nextButton = document.getElementById("next-button");
 let prevButton = document.getElementById("prev-button");
-let div1 = document.getElementById("div1");
-let div2 = document.getElementById("div2");
-let div3 = document.getElementById("div3");
-let div4 = document.getElementById("div4");
+let bullet1 = document.getElementById("bullet1");
+let bullet2 = document.getElementById("bullet2");
+let bullet3 = document.getElementById("bullet3");
+let bullet4 = document.getElementById("bullet4");
+let content1 = document.getElementById("content1");
+let content2 = document.getElementById("content2");
+let content3 = document.getElementById("content3");
+let content4 = document.getElementById("content4");
 let currentDiv = 0;
 
-div1.style.display = "block";
+bullet1.style.backgroundColor = "#673AB7";
+content1.style.display = "block";
 
 nextButton.addEventListener("click", function() {
     let current = document.getElementsByClassName("content-div")[currentDiv];
     current.style.display = "none";
+    let currentBullet = document.getElementsByClassName("bullet")[currentDiv];
+    currentBullet.style.backgroundColor = "#673AB7";
     currentDiv = (currentDiv + 1) % 4;
     current = document.getElementsByClassName("content-div")[currentDiv];
     current.style.display = "block";
+    currentBullet = document.getElementsByClassName("bullet")[currentDiv];
+    currentBullet.style.backgroundColor = "#673AB7";
 });
-prevButton.addEventListener("click", function() {
-    let current = document.getElementsByClassName("content-div")[currentDiv];
-    current.style.display = "none";
-    currentDiv = (currentDiv - 1 + 4) % 4;
-    current = document.getElementsByClassName("content-div")[currentDiv];
-    current.style.display = "block";
-});
+
+var imageIndex = 0;
+var images = ["/assets/img/35.jpg", "/assets/img/93.jpg", "/assets/img/39.jpg", "/assets/img/51.jpg"];
+
+setInterval(function() {
+    document.getElementById("content-img").style.backgroundImage = `url(${images[imageIndex]})`;
+    imageIndex = (imageIndex + 1) % images.length;
+}, 5000);
