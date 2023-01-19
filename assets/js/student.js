4f2bebab9b25f46bb05dee09bbf45a9120e96eca
ismailaib <ismailaitbouhmad@gmail.com>
2023-01-19 22:23:13 +0100

    add code

--- assets/js/student.js
+++ assets/js/student.js
@@ -1,4 +1,18 @@
-
+$(document).ready(function(){
+    $("#mysettings").click(function(){
+      $(".settings").show();
+      $(".request, .progress, .text").hide();
+    });
+    $("#myrequest").click(function(){
+      $(".request").show();
+      $(".settings, .progress, .text").hide();
+    });
+    $("#myprogress").click(function(){
+      $(".progress").show();
+      $(".settings, .request, .text").hide();
+    });
+  });
+  
 
 
 
